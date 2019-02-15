71330f92dd6ec85bda7f0c8a3b874ec762e2e81b
Anthony Viglione <viglione2014.2015@gmail.com>
2019-02-15 12:34:48 -0700

    connected database to server file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
+const massive = require('massive')
 
 const app =express();
 
 app.use(bodyParser.json());
 
+const {CONNECTION_STRING} = process.env
+// console.log(CONNECTION_STRING)
 
-
-app.listen(3001,()=>{console.log('Ready to Rock on port 3001!')})
+massive(CONNECTION_STRING).then((dbInstance)=>{
+    app.set('db',dbInstance)
+    app.listen(3001,()=>{console.log('Ready to Rock on port 3001!')})
+})
